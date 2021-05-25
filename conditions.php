ef1630776f2d9222d5dd5037ce2b3f043904507e
Kieran Brahney <kieran@supportpal.com>
2021-05-25 14:49:40 +0100

    fix indentation

--- conditions.php
+++ conditions.php
@@ -4,98 +4,98 @@ return array(
     /*
      * 2.0.2
      */
-    "assigned_operator"                => "Назначенный оператор",
-    "assigned_operator_group"          => "Назначенный оператор группы",
-    "hours_since_created"              => "Часы с момента создания",
-    "hours_since_updated"              => "Часы с момента обновления",
-    "internal_ticket"                  => "Внутренний тикет",
-    "involved_operator"                => "Участвующий оператор",
-    "last_ticket_replier"              => "Последний ответ в тикете",
-    "ticket_channel"                   => "Канал запросов",
-    "ticket_channel_account"           => "Аккаунт канала запросов",
-    "ticket_created"                   => "Запрос создан",
-    "ticket_department"                => "Отдел тикетов",
-    "ticket_due_time"                  => "Срок исполнения тикетов",
-    "ticket_message_content"           => "Содержание сообщения тикета",
-    "ticket_message_count"             => "Количество сообщений в тикете",
-    "ticket_priority"                  => "Приоритет тикета",
-    "ticket_sla_plan"                  => "План тикета SLA",
-    "ticket_status"                    => "Статус тикета",
-    "ticket_subject"                   => "Тема тикета",
-    "ticket_tag"                       => "Тег тикета",
-    "ticket_updated"                   => "Тикет обновлен",
-    "user_email"                       => "Электронная почта пользователя",
-    "user_full_name"                   => "Полное имя пользователя",
-    "user_group"                       => "Группа пользователей",
+    "assigned_operator"                 => "Назначенный оператор",
+    "assigned_operator_group"           => "Назначенный оператор группы",
+    "hours_since_created"               => "Часы с момента создания",
+    "hours_since_updated"               => "Часы с момента обновления",
+    "internal_ticket"                   => "Внутренний тикет",
+    "involved_operator"                 => "Участвующий оператор",
+    "last_ticket_replier"               => "Последний ответ в тикете",
+    "ticket_channel"                    => "Канал запросов",
+    "ticket_channel_account"            => "Аккаунт канала запросов",
+    "ticket_created"                    => "Запрос создан",
+    "ticket_department"                 => "Отдел тикетов",
+    "ticket_due_time"                   => "Срок исполнения тикетов",
+    "ticket_message_content"            => "Содержание сообщения тикета",
+    "ticket_message_count"              => "Количество сообщений в тикете",
+    "ticket_priority"                   => "Приоритет тикета",
+    "ticket_sla_plan"                   => "План тикета SLA",
+    "ticket_status"                     => "Статус тикета",
+    "ticket_subject"                    => "Тема тикета",
+    "ticket_tag"                        => "Тег тикета",
+    "ticket_updated"                    => "Тикет обновлен",
+    "user_email"                        => "Электронная почта пользователя",
+    "user_full_name"                    => "Полное имя пользователя",
+    "user_group"                        => "Группа пользователей",
 
     /*
      * 2.1.0
      */
-    "ticket_brand"                     => "Марка тикета",
-    "user_brand"                       => "Марка пользователя",
+    "ticket_brand"                      => "Марка тикета",
+    "user_brand"                        => "Марка пользователя",
 
     /*
      * 2.2.0
      */
-    "hours_since_last_reply"           => "Часов с последнего ответа",
-    "hours_since_last_reply_exc_notes" => "Часы с момента последнего ответа (исключая заметки)",
-    "hours_until_due"                  => "Часов до",
-    "last_ticket_replier_exc_notes"    => "Последний ответчик тикета (исключая примечания)",
-    "ticket_department_email"          => "Электронная почта отдела тикетов",
-    "ticket_has_attachments"           => "Тикет имеет вложения",
-    "ticket_has_drafts"                => "Тикет имеет черновики",
-    "ticket_locked"                    => "Тикет заблокирован",
-    "ticket_merged"                    => "Тикет объединен",
-    "ticket_message_count_exc_notes"   => "Количество сообщений в тикете (за исключением заметок)",
-    "ticket_note_count"                => "Количество заметок в тикете",
-    "ticket_number"                    => "Номер тикета",
-    "ticket_reopened_time"             => "Время открытия тикета",
-    "ticket_resolved_time"             => "Время решения тикета",
-    "user_account_active"              => "Аккаунт пользователя активен",
-    "user_country"                     => "Страна пользователя",
-    "user_language"                    => "Язык пользователя",
-    "user_timezone"                    => "Часовой пояс пользователя",
+    "hours_since_last_reply"            => "Часов с последнего ответа",
+    "hours_since_last_reply_exc_notes"  => "Часы с момента последнего ответа (исключая заметки)",
+    "hours_until_due"                   => "Часов до",
+    "last_ticket_replier_exc_notes"     => "Последний ответчик тикета (исключая примечания)",
+    "ticket_department_email"           => "Электронная почта отдела тикетов",
+    "ticket_has_attachments"            => "Тикет имеет вложения",
+    "ticket_has_drafts"                 => "Тикет имеет черновики",
+    "ticket_locked"                     => "Тикет заблокирован",
+    "ticket_merged"                     => "Тикет объединен",
+    "ticket_message_count_exc_notes"    => "Количество сообщений в тикете (за исключением заметок)",
+    "ticket_note_count"                 => "Количество заметок в тикете",
+    "ticket_number"                     => "Номер тикета",
+    "ticket_reopened_time"              => "Время открытия тикета",
+    "ticket_resolved_time"              => "Время решения тикета",
+    "user_account_active"               => "Аккаунт пользователя активен",
+    "user_country"                      => "Страна пользователя",
+    "user_language"                     => "Язык пользователя",
+    "user_timezone"                     => "Часовой пояс пользователя",
 
     /*
      * 2.3.0
      */
-    "ticket_cc"                        => "Тикет CC",
-    "ticket_custom_field"              => "Пользовательское поле тикета",
-    "user_custom_field"                => "Пользовательское поле пользователя",
-    "user_organisation_custom_field"   => "Пользовательское поле организации пользователя",
-    "user_organisation"                => "Название организации пользователя",
-    "user_organisation_brand"          => "Пользовательская организация бренда",
-    "user_organisation_country"        => "Страна организации пользователя",
-    "user_organisation_language"       => "Язык организации пользователя",
-    "user_organisation_notes"          => "Замечания по организации пользователей",
-    "user_organisation_timezone"       => "Часовой пояс организации пользователя",
+    "ticket_cc"                         => "Тикет CC",
+    "ticket_custom_field"               => "Пользовательское поле тикета",
+    "user_custom_field"                 => "Пользовательское поле пользователя",
+    "user_organisation_custom_field"    => "Пользовательское поле организации пользователя",
+    "user_organisation"                 => "Название организации пользователя",
+    "user_organisation_brand"           => "Пользовательская организация бренда",
+    "user_organisation_country"         => "Страна организации пользователя",
+    "user_organisation_language"        => "Язык организации пользователя",
+    "user_organisation_notes"           => "Замечания по организации пользователей",
+    "user_organisation_timezone"        => "Часовой пояс организации пользователя",
 
     /*
      * 2.4.0
      */
-    "watching_operator"                => "Ожидание оператора",
-    "exact_search_quotes"              => "Чтобы выполнить точный поиск фразы, заключите ее в кавычки, например \"моя фраза\".",
+    "watching_operator"                 => "Ожидание оператора",
+    "exact_search_quotes"               => "Чтобы выполнить точный поиск фразы, заключите ее в кавычки, например \"моя фраза\".",
 
     /*
      * 2.4.1
      */
-    "last_message_content"             => "Содержание последнего сообщения",
+    "last_message_content"              => "Содержание последнего сообщения",
 
     /*
      * 2.5.0
      */
-    "user_email_confirmed"             => "Электронный адрес пользователя подтвержден",
+    "user_email_confirmed"              => "Электронный адрес пользователя подтвержден",
 
     /*
      * 3.0.0
      */
-    "user_2fa_enabled"                 => "2FA активирована",
-    "article_type"                     => "Article type",
-    "article_brand"                    => "Article brand",
+    "user_2fa_enabled"                  => "2FA активирована",
+    "article_type"                      => "Article type",
+    "article_brand"                     => "Article brand",
 
     /*
      * 3.3.0
      */
-    "user_has_organisation"            => "User has organisation",
+    "user_has_organisation"             => "User has organisation",
 
 );
